d6bc90aa9870d37748c6c29d8c87e5679ebe0f97
thaidinh <thai562003@gmail.com>
2024-08-15 23:23:52 +0700

    update

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,5 @@
 const images = {
-    logo: require('~/assets/images/takatech-logo.png'),
+    logo: `${process.env.PUBLIC_URL}/static/media/takatech-logo.764b8f03065219c38a1b.png`,
 };
 
 export default images;
